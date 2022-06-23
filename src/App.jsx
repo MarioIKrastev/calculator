30459be7b2df4ef990c6a3d4d351a420249c8591
unknown <mario.krastev@mentormate.com>
2022-06-23 14:46:45 +0300

    Add corrections

--- src/App.jsx
+++ src/App.jsx
@@ -2,90 +2,8 @@ import { useReducer } from "react";
 import DigitButton from "./DigitBtn";
 import OperationButton from "./OperationButton";
 import { ACTIONS } from "./Actions";
+import reducer from "./reducer";
 import "./styles.css";
-
-function reducer(state, { type, payload }) {
-  switch (type) {
-    case ACTIONS.ADD_DIGIT:
-      if (state.overwrite) {
-        return {
-          ...state,
-          currentOperand: payload.digit,
-          overwrite: false,
-        };
-      }
-      if (payload.digit === "0" && state.currentOperand === "0") return state;
-      if (payload.digit === "." && state.currentOperand.includes("."))
-        return state;
-      return {
-        ...state,
-        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
-      };
-    case ACTIONS.CHOOSE_OPERATIONS:
-      if (state.currentOperand == null && state.previousOperand == null) {
-        return state;
-      }
-
-      if (state.currentOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-        };
-      }
-      if (state.previousOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-          previousOperand: state.currentOperand,
-          currentOperand: null,
-        };
-      }
-      return {
-        ...state,
-        previousOperand: evaluate(state),
-        operation: payload.operation,
-        currentOperand: null,
-      };
-    case ACTIONS.CLEAR:
-      return {};
-    case ACTIONS.EVALUATE:
-      if (
-        state.operation == null ||
-        state.previousOperand == null ||
-        state.operation == null
-      ) {
-        return state;
-      }
-      return {
-        ...state,
-        overwrite: true,
-        operation: null,
-        previousOperand: null,
-        currentOperand: evaluate(state),
-      };
-    case ACTIONS.REMOVE_DIGIT:
-      if (state.overwrite) {
-        return {
-          ...state,
-          overwrite: false,
-          currentOperand: null,
-        };
-      }
-      if (state.currentOperand == null) return state;
-      if (state.currentOperand.length === 1) {
-        return {
-          ...state,
-          currentOperand: null,
-        };
-      }
-      return {
-        ...state,
-        currentOperand: state.currentOperand.slice(0, -1),
-      };
-    default:
-      break;
-  }
-}
 
 const INT_FORMATER = new Intl.NumberFormat("en-us", {
   maximumFractionDigits: 0,
@@ -95,10 +13,10 @@ function formatOperand(operand) {
   if (operand == null) return;
   const [integer, decimal] = operand.split(".");
   if (decimal == null) return INT_FORMATER.format(integer);
-  return `${INT_FORMATER.format(integer)}.${decimal}}`;
+  return `${INT_FORMATER.format(integer)}.${decimal}`;
 }
 
-function evaluate({ currentOperand, previousOperand, operation }) {
+export function evaluate({ currentOperand, previousOperand, operation }) {
   const prev = parseFloat(currentOperand);
   const current = parseFloat(previousOperand);
   if (isNaN(prev) || isNaN(current)) return "";
---
+++ src/reducer.jsx
@@ -0,0 +1,85 @@
+import { ACTIONS } from "./Actions";
+import { evaluate } from "./App";
+
+export default function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
+      if (payload.digit === "." && state.currentOperand.includes("."))
+        return state;
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+    case ACTIONS.CHOOSE_OPERATIONS:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+    case ACTIONS.CLEAR:
+      return {};
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation == null ||
+        state.previousOperand == null ||
+        state.operation == null
+      ) {
+        return state;
+      }
+      return {
+        ...state,
+        overwrite: true,
+        operation: null,
+        previousOperand: null,
+        currentOperand: evaluate(state),
+      };
+    case ACTIONS.REMOVE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null,
+        };
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+    default:
+      break;
+  }
+}
